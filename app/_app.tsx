b54ca674fd347afd49336a37e51cc32c5c6d133e
Dixon Yant <dyant@esri.com>
2025-11-12 14:27:47 -0800

    app view init

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,1 +1,7 @@
-import "./global.css"
+
+import "../global.css";
+import { Slot } from "expo-router";
+
+export default function App() {
+	return <Slot />;
+}
---
+++ app/leafletcdn.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function LeafletCDN() {
+	return (
+		<View className="flex-1 items-center justify-center bg-gray-200">
+			<Text className="text-gray-500">Leaflet CDN Map will go here</Text>
+		</View>
+	);
+}
