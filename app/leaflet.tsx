369f6c2a87ddc6bfd6612255075791f05674cb59
Dixon Yant <dyant@esri.com>
2025-11-17 10:24:48 -0800

    implement react native leaflet view component rc

--- app/leaflet.tsx
+++ app/leaflet.tsx
@@ -6,10 +6,10 @@ import { LeafletView } from "react-native-leaflet-view";
 import { leafletMapStyle1, leafletMapStyle2 } from "./maps/mapstyles";
 
 // https://www.npmjs.com/package/react-native-leaflet-view
-const DEFAULT_LOCATION = {
-  latitude: -23.5489,
-  longitude: -46.6388
-};
+// const DEFAULT_LOCATION = {
+//   latitude: 23.5489,
+//   longitude: 46.6388
+// };
 export default function Leaflet() {
   const router = useRouter();
   const key = process.env.EXPO_PUBLIC_ARCGIS_API_KEY;
@@ -44,10 +44,6 @@ export default function Leaflet() {
   return (
     <LeafletView
       source={{ html: webViewContent }}
-      mapCenterPosition={{
-        lat: DEFAULT_LOCATION.latitude,
-        lng: DEFAULT_LOCATION.longitude,
-      }}
     />
   );
 }
