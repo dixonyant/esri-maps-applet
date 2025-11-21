d5974d0f62bc512727bd99444e8735d6284b236f
Dixon Yant <dyant@esri.com>
2025-11-21 12:51:07 -0800

    change background

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,44 @@
-import { Text, View, Pressable } from "react-native";
-import { useRouter } from "expo-router";
-
+import { Text, View, Pressable, ImageBackground } from "react-native";
+import { Stack, useRouter } from "expo-router";
 export default function Home() {
   const router = useRouter();
   return (
-    <View className="flex-1 bg-white items-center justify-center">
-      <View className="w-full max-w-xs items-center">
-        <Pressable
-          className="w-full mb-4 px-4 py-3 bg-blue-500 rounded items-center"
-          onPress={() => router.push("/leaflet")}
+    <>
+      <Stack.Screen
+        options={{
+          title: "",
+          headerStyle: { backgroundColor: "black" },
+          headerTintColor: "white",
+        }}
+      />
+      <ImageBackground
+        source={{
+          uri: "https://www.esri.com/arcgis-blog/wp-content/uploads/2021/08/Nighttime-Lights-Globe-EastHem2.jpg",
+        }}
+        style={{ flex: 1 }}
+        resizeMode="cover"
+      >
+        <View
+          className="flex-1 justify-center items-center bg-black/50"
         >
-          <Text className="text-white font-medium text-lg">Leaflet Component</Text>
-        </Pressable>
-        <Pressable
-          className="w-full mb-4 px-4 py-3 bg-purple-600 rounded items-center"
-          onPress={() => router.push("/leafletcdn")}
-        >
-          <Text className="text-white font-medium text-lg">Leaflet CDN</Text>
-        </Pressable>
-        <Pressable
-          className="w-full px-4 py-3 bg-green-600 rounded items-center"
-          onPress={() => router.push("/maplibre")}
-        >
-          <Text className="text-white font-medium text-lg">MapLibre</Text>
-        </Pressable>
-      </View>
-    </View>
+          <View className="w-full max-w-xs items-center">
+            <Pressable
+              className="w-full mb-4 px-4 py-3 bg-purple-600 rounded items-center"
+              onPress={() => router.push("/leafletcdn")}
+            >
+              <Text className="text-white font-medium text-lg">
+                Leaflet CDN
+              </Text>
+            </Pressable>
+            <Pressable
+              className="w-full px-4 py-3 bg-green-600 rounded items-center"
+              onPress={() => router.push("/maplibre")}
+            >
+              <Text className="text-white font-medium text-lg">MapLibre</Text>
+            </Pressable>
+          </View>
+        </View>
+      </ImageBackground>
+    </>
   );
 }
